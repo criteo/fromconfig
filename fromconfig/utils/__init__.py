bad942733210b302ec5e25fe7b95b8752d2a6343
Guillaume Genthial <g.genthial@criteo.com>
2021-04-19 23:18:40 +0200

    Add testing utility for external launchers.

--- fromconfig/utils/__init__.py
+++ fromconfig/utils/__init__.py
@@ -4,3 +4,4 @@ from fromconfig.utils.types import is_mapping, is_pure_iterable
 from fromconfig.utils.libimport import from_import_string, to_import_string, try_import
 from fromconfig.utils.strenum import StrEnum
 from fromconfig.utils.nest import flatten, expand, merge_dict, depth_map
+import fromconfig.utils.testing
---
+++ fromconfig/utils/testing.py
@@ -0,0 +1,8 @@
+"""Utilities for testing."""
+
+
+def assert_launcher_is_discovered(name: str, expected):
+    """Test that a launcher is found by name."""
+    # pylint: disable=import-outside-toplevel
+    from fromconfig.launcher.base import _get_cls
+    assert _get_cls(name) is expected
