1a97e984b6045f26651c517d05fb1d4cc2e447e8
Guillaume Genthial <g.genthial@criteo.com>
2021-04-19 19:15:08 +0200

    Undo changes in quickstart

--- docs/examples/quickstart/foo.py
+++ docs/examples/quickstart/foo.py
@@ -1,7 +1,5 @@
 """Dummy Model."""
 
-import mlflow
-
 
 class Model:
     def __init__(self, learning_rate: float):
@@ -9,5 +7,3 @@ class Model:
 
     def train(self):
         print(f"Training model with learning_rate {self.learning_rate}")
-        if mlflow.active_run() is not None:
-            mlflow.log_metric("loss", self.learning_rate)
